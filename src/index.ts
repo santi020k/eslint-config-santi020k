8d02386a1d90a3ee7efe5ef0e08ffdc492a62be7
Santiago Molina Orozco <santi020k@gmail.com>
2024-06-02 18:32:00 -0500

    fix(src/index.ts): eslint auto fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { jsConfig as jsEslint } from './js/index.config.ts'
+import { nextConfig as nextEslint } from './next/index.config.ts'
+import { nextTsConfig as nextTsEslint } from './next-ts/index.config.ts'
 import { reactConfig as reactEslint } from './react/index.config.ts'
 import { reactTsConfig as reactTsEslint } from './react-ts/index.config.ts'
 import { tsConfig as tsEslint } from './ts/index.config.ts'
-import { nextConfig as nextEslint } from './next/index.config.ts'
-import { nextTsConfig as nextTsEslint } from './next-ts/index.config.ts'
 
 export { jsEslint, tsEslint, reactEslint, reactTsEslint, nextEslint, nextTsEslint }
