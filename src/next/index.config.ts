e493b2697f6a68fc92ec38e874e0b8ae43c39a38
Santiago Molina Orozco <santi020k@gmail.com>
2024-06-02 21:11:42 -0500

    fix(index.config.ts): invert react and next order

--- src/next/index.config.ts
+++ src/next/index.config.ts
@@ -6,8 +6,8 @@ import { rules } from './rules.ts'
 import { fixupConfigRules } from '@eslint/compat'
 
 const nextConfig = [
-  ...fixupConfigRules(flatCompat.extends('plugin:@next/next/core-web-vitals')),
   ...reactConfig,
+  ...fixupConfigRules(flatCompat.extends('plugin:@next/next/core-web-vitals')),
   {
     name: 'custom-next',
     rules
